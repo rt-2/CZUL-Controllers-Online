7d169159dedf0167fbff6c8910049c6b18cc1175
rt-2 <rt-2@rt-2.net>
2019-10-27 01:48:58 -0400

    update

--- ControllersOnline/index.php
+++ ControllersOnline/index.php
@@ -23,10 +23,15 @@
 
     include_once('resources/fir.lib.inc.php');
     include_once('resources/api.lib.inc.php');
+    include_once('resources/actypes.lib.data.php');
+
+    //var_dump($acTypes);
+    //echo '<textarea>';
+    //echo json_encode($acTypes);
+    //echo '</textarea>';
 
 	$source_url = 'http://us.data.vatsim.net/vatsim-data.txt';
-	$fetch_result = file_get_contents($source_url);
-//var_dump($fetch_result);
+	$fetch_result = curl_get_contents($source_url);
 	preg_match('/(?<=^\!CLIENTS\:\n)(.*)(?=^\;$\n^\;$)/msU', $fetch_result, $matches);
 
 	
@@ -34,7 +39,64 @@
 	
 	//var_dump($all_clientsRaw);
 	//var_dump($clientColumns);
+
+
+
+    function GetTooltipHTML($text, $tooltipText)
+    {
+        $return_str = '';
+        $return_str .= '<div class="tooltip">';
+        $return_str .= $text;
+        if(strlen($tooltipText) > 0)
+        {
+            $return_str .= '<span class="tooltiptext">';
+            $return_str .= $tooltipText;
+            $return_str .= '</span>';
+        }
+        $return_str .= '</div>';
+        return $return_str;
+    }
+    function GetAircraftTypeHTML($actypeStr)
+    {
+        $acName = '';
+
+        preg_match("/^(?>[A-Z]?\/)?(\w{2,6})(?>\/[A-Z]?)?$/", $actypeStr, $matches);
+        $aircraft = strtoupper($matches[1]);
+
+        foreach(AC_TYPES as $acType)
+        {
+            if($acType['icao'] === $aircraft)
+            {
+                $acName = $acType['name'];
+            }
+        }
+
+        return GetTooltipHTML($aircraft, $acName);
+    }
+    function curl_get_contents($url)
+    {
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      $data = curl_exec($ch);
+      curl_close($ch);
+      return $data;
+    }
+
+
+
+
+
+
 	
+	foreach($firAirports as $icao => $name)
+	{
+        new FirAirport($icao, $name);
+    }
+
+
 	$all_atcs = []; $all_pilots = [];
 	$online_atcs = []; $inbound_pilots = []; $outbound_pilots = [];
 	foreach($all_clientsRaw as $clientRaw)
@@ -80,8 +142,8 @@
 		)
 		{
 			//echo "\n".urldecode(json_encode($pilot, JSON_PRETTY_PRINT))."\n";
-			$firArport = FirAirport::$all_list[$pilot_dep];
-			$firArport->outbound_list[] = $pilot;
+			$firAirport = FirAirport::$all_list[$pilot_dep];
+			$firAirport->outbound_list[] = $pilot;
 			$outbound_pilots[] = $pilot;
 		}
 		if(
@@ -89,8 +151,8 @@
 		)
 		{
 			//echo "\n".urldecode(json_encode($pilot, JSON_PRETTY_PRINT))."\n";
-			$firArport = FirAirport::$all_list[$pilot_arr];
-			$firArport->inbound_list[] = $pilot;
+			$firAirport = FirAirport::$all_list[$pilot_arr];
+			$firAirport->inbound_list[] = $pilot;
 			$inbound_pilots[] = $pilot;
 		}
 	}
@@ -138,6 +200,41 @@
 		table {
 			color: rgb(75, 75, 75);
 		}
+	</style>
+	<style type="text/css">
+        .tooltip {
+          position: relative;
+          display: inline-block;
+        }
+
+        .tooltip .tooltiptext {
+          visibility: hidden;
+          background-color: black;
+          color: #fff;
+          text-align: center;
+          padding: 5px 10px;
+          position: absolute;
+          z-index: 1;
+          top: 150%;
+          left: 50%;
+          margin-left: -60px;
+        white-space: nowrap;
+        }
+
+        .tooltip .tooltiptext::after {
+          content: "";
+          position: absolute;
+          bottom: 100%;
+          left: 50%;
+          margin-left: -5px;
+          border-width: 5px;
+          border-style: solid;
+          border-color: transparent transparent black transparent;
+        }
+
+        .tooltip:hover .tooltiptext {
+          visibility: visible;
+        }
 	</style>
 	
 </head>
@@ -199,9 +296,23 @@
 					{
 						?>
 						<tr>
-							<td><?=$pilot['planned_aircraft']?></td>
-							<td><?=$pilot['callsign']?></td>
-							<td><?=$pilot['planned_depairport']?>&nbsp;->&nbsp;<?=$pilot['planned_destairport']?></td>
+							<td>
+                                <?=GetAircraftTypeHTML($pilot['planned_aircraft'])?>
+                            </td>
+							<td>
+                                <?=GetTooltipHTML($pilot['callsign'], $pilot['realname'])?>
+                            </td>
+							<td>
+                                <div class="tooltip">
+                                    <?=$pilot['planned_depairport']?>
+                                    <span class="tooltiptext"><?=$pilot['realname']?></span>
+                                </div>
+                                &nbsp;->&nbsp;
+                                <div class="tooltip">
+                                    <?=$pilot['planned_destairport']?>
+                                    <span class="tooltiptext"><?=$pilot['realname']?></span>
+                                </div>
+                            </td>
 						</tr>
 						<?php
 					}
@@ -236,9 +347,17 @@
 					{
 						?>
 						<tr>
-							<td><?=$pilot['planned_aircraft']?></td>
-							<td><?=$pilot['callsign']?></td>
-							<td><?=$pilot['planned_depairport']?>&nbsp;->&nbsp;<?=$pilot['planned_destairport']?></td>
+							<td>
+                                <?=GetAircraftTypeHTML($pilot['planned_aircraft'])?>
+                            </td>
+							<td>
+                                <?=GetTooltipHTML($pilot['callsign'], $pilot['realname'])?>
+                            </td>
+							<td>
+                                <?=$pilot['planned_depairport']?>
+                                &nbsp;->&nbsp;
+                                <?=$pilot['planned_destairport']?>
+                            </td>
 						</tr>
 						<?php
 					}
